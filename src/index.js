0abeb86791e260139bbdcc2bf7a6b242926f14b9
Steve Godin <steve.god@gmail.com>
2018-11-09 13:54:21 -0800

    Add demo for QuestionDetails

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
+// A React Component is a function that returns a React element.
+// A React element is the object returned by the function
+// React.createElement(...). The object is minimal version of a
+// a DOM node containing the bare minimum properties.
 
-ReactDOM.render(<p>Stuff</p>, document.getElementById("root"));
+// Your React components must be written as PascalCase. That is capitalized
+// camelCase. Otherwise, React will treat it as an HTML tag and not render your
+// component.
+const QuestionDetails = () => (
+  <div className="QuestionDetails">
+    <h2>What is your favourite colour?</h2>
+    <p>Red, green, blue, magenta, yellow, etc.</p>
+    <p>
+      <small>Seen 10 time(s)</small>•<small>Created 10 days ago</small>•
+      <small>Last edited 2 hours ago</small>
+    </p>
+  </div>
+);
+
+ReactDOM.render(<QuestionDetails />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
